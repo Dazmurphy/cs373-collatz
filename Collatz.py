80ab6907f820049e2c243b50607d139c93bf2f6d
Dazmurphy <daz.murphy.94@gmail.com>
2016-01-29 22:05:50 -0600

    added comments describing steps to complete

---
+++ Collatz.py
@@ -0,0 +1,13 @@
+
+#take in input
+#split two numbers into 2 variables
+#lOOP_1
+#loop through values between 2 variables
+#LOOP_2
+#check if even
+#then divide by 2
+#check if odd
+#then multiply by 3 and 1
+#if 1 then break loop
+#LOOP_2
+#LOOP_1
