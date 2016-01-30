e7d7eb7174aae17e331b830f1647ebb8149e421f
Dazmurphy <daz.murphy.94@gmail.com>
2016-01-30 17:53:19 -0600

    fixed issue of split not working properly closes #11

--- Collatz.py
+++ Collatz.py
@@ -1,3 +1,4 @@
+#Author: Darragh Murphy
 
 #take in input
 #split two numbers into 2 variables
@@ -11,3 +12,17 @@
 #if 1 then break loop
 #LOOP_2
 #LOOP_1
+
+user_input = input('Enter two digits seperated by a space: ')
+print(user_input)
+user_input = user_input.split(' ')
+i = int(user_input[0])
+j = int(user_input[1])
+
+while i != 1:
+    if i % 2 == 0:
+        i = i // 2
+        print(i)
+    else:
+        i = i * 3 + 1
+        print(i)
