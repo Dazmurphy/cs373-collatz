04cc8897d88dfae6f34799f768b82c3578aa2a4f
Dazmurphy <daz.murphy.94@gmail.com>
2016-02-01 00:23:01 -0600

    fixed unit tests

--- TestCollatz.py
+++ TestCollatz.py
@@ -26,32 +26,62 @@ class TestCollatz (TestCase) :
     # read
     # ----
 
-    def test_read (self) :
+    def test_read_1 (self) :
         s    = "1 10\n"
         i, j = collatz_read(s)
         self.assertEqual(i,  1)
         self.assertEqual(j, 10)
 
+    def test_read_2 (self) :
+        s = "1 1\n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 1)
+        self.assertEqual(j, 1)
+
+    def test_read_3 (self) :
+        s = "50000  23 \n"
+        i, j = collatz_read(s)
+        self.assertEqual(i, 50000)
+        self.assertEqual(j, 23)
     # ----
     # eval
     # ----
 
     def test_eval_1 (self) :
         v = collatz_eval(1, 10)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 19)
 
     def test_eval_2 (self) :
         v = collatz_eval(100, 200)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 124)
 
     def test_eval_3 (self) :
         v = collatz_eval(201, 210)
-        self.assertEqual(v, 1)
+        self.assertEqual(v, 88)
 
     def test_eval_4 (self) :
         v = collatz_eval(900, 1000)
+        self.assertEqual(v, 173)
+
+    def test_eval_5 (self) :
+        v = collatz_eval(1, 1)
+        self.assertEqual(v, 0)
+
+    def test_eval_5 (self) :
+        v = collatz_eval(1, 50000)
+        self.assertEqual(v, 323)
+
+    def test_eval_6 (self) :
+        v = collatz_eval(533, 503)
+        self.assertEqual(v, 123)
+
+    def test_eval_7 (self) :
+        v = collatz_eval(2, 1)
         self.assertEqual(v, 1)
 
+    def test_eval_8 (self) :
+        v = collatz_eval(24532, 50000)
+        self.assertEqual(v, 323)
     # -----
     # print
     # -----
@@ -69,7 +99,7 @@ class TestCollatz (TestCase) :
         r = StringIO("1 10\n100 200\n201 210\n900 1000\n")
         w = StringIO()
         collatz_solve(r, w)
-        self.assertEqual(w.getvalue(), "1 10 1\n100 200 1\n201 210 1\n900 1000 1\n")
+        self.assertEqual(w.getvalue(), "1 10 19\n100 200 124\n201 210 88\n900 1000 173\n")
 
 # ----
 # main
